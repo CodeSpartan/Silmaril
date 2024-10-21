69cf624f75554fd551abc576cfae28c7896cb178
CodeSpartan <codespartan.supp@gmail.com>
2024-10-22 01:17:14 +0300

    additional windows work

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,26 +4,33 @@ import model.MudConnection
 import view.MainWindow
 import viewmodel.MainViewModel
 import viewmodel.MapViewModel
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.window.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.window.WindowDraggableArea
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.DialogWindow
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.awt.ComposeDialog
+import java.awt.Dimension
+import java.awt.event.WindowAdapter
+import java.awt.event.WindowEvent
 
 fun main() = application {
     val client = MudConnection("adan.ru", 4000)
     val mainViewModel = MainViewModel(client)
     val mapViewModel = MapViewModel(client)
 
-    val showSecondaryWindow = remember { mutableStateOf(true) }
+    val showMapWindow = remember { mutableStateOf(true) }
+    val showAdditionalOutputWindow = remember { mutableStateOf(true) }
 
     // Main Window
     Window(
@@ -38,7 +45,10 @@ fun main() = application {
         MenuBar {
             Menu("File") {
                 Item("Toggle Map") {
-                    showSecondaryWindow.value = !showSecondaryWindow.value
+                    showMapWindow.value = !showMapWindow.value
+                }
+                Item("Toggle Additional Output") {
+                    showAdditionalOutputWindow.value = !showAdditionalOutputWindow.value
                 }
                 Item("Exit") {
                     mainViewModel.cleanup()
@@ -47,10 +57,68 @@ fun main() = application {
                 }
             }
         }
+
+
         MainWindow(mainViewModel)
 
-        if (showSecondaryWindow.value) {
-            SecondaryWindow { showSecondaryWindow.value = false } // becomes false if it gets closed
+        if (showMapWindow.value) {
+            DialogWindow(
+                create = {
+                    ComposeDialog(owner = window).apply {
+                        size = Dimension(300, 200)
+                        isFocusable = true
+                        isUndecorated = true
+                        setLocation(300, 300)
+
+                        addWindowListener(object : WindowAdapter() {
+                            override fun windowClosing(e: WindowEvent) {
+                                showMapWindow.value = false
+                                println("Dialog is closing")
+                            }
+                        })
+                    }
+                },
+                dispose = ComposeDialog::dispose
+            ) {
+                Column(Modifier.background(Color(0xFFEEEEEE))) {
+                    AppWindowTitleBar()
+                    Row {
+                        Text("label 1", Modifier.size(100.dp, 100.dp).padding(10.dp).background(Color.White))
+                        Text("label 2", Modifier.size(150.dp, 200.dp).padding(5.dp).background(Color.White))
+                        Text("label 3", Modifier.size(200.dp, 300.dp).padding(25.dp).background(Color.White))
+                    }
+                }
+            }
+        }
+
+        if (showAdditionalOutputWindow.value) {
+            DialogWindow(
+                create = {
+                    ComposeDialog(owner = window).apply {
+                        size = Dimension(300, 200)
+                        isFocusable = true
+                        isUndecorated = true
+                        setLocation(300, 500)
+
+                        addWindowListener(object : WindowAdapter() {
+                            override fun windowClosing(e: WindowEvent) {
+                                showAdditionalOutputWindow.value = false
+                                println("Dialog is closing")
+                            }
+                        })
+                    }
+                },
+                dispose = ComposeDialog::dispose,
+            ) {
+                Column(Modifier.background(Color(0xFFEEEEEE))) {
+                    AppWindowTitleBar()
+                    Row {
+                        Text("label 1", Modifier.size(100.dp, 100.dp).padding(10.dp).background(Color.White))
+                        Text("label 2", Modifier.size(150.dp, 200.dp).padding(5.dp).background(Color.White))
+                        Text("label 3", Modifier.size(200.dp, 300.dp).padding(25.dp).background(Color.White))
+                    }
+                }
+            }
         }
     }
 
@@ -62,30 +130,6 @@ fun main() = application {
 }
 
 @Composable
-fun SecondaryWindow(onClose: () -> Unit) {
-    Window(
-        title = "Silmaril Map",
-        icon = painterResource("icon.png"),
-        onCloseRequest = onClose,
-        undecorated = true,
-        state = rememberWindowState(width = 300.dp, height = 400.dp),
-        resizable = false,
-        alwaysOnTop = true
-    ) {
-        // Define UI within the secondary window
-        Column(Modifier.background(Color(0xFFEEEEEE))) {
-            AppWindowTitleBar()
-            Row {
-                Text("label 1", Modifier.size(100.dp, 100.dp).padding(10.dp).background(Color.White))
-                Text("label 2", Modifier.size(150.dp, 200.dp).padding(5.dp).background(Color.White))
-                Text("label 3", Modifier.size(200.dp, 300.dp).padding(25.dp).background(Color.White))
-            }
-        }
-    }
-}
-
-
-@Composable
 private fun WindowScope.AppWindowTitleBar() = WindowDraggableArea {
-    Box(Modifier.fillMaxWidth().height(48.dp).background(Color.DarkGray))
+    Box(Modifier.fillMaxWidth().height(15.dp).background(Color.DarkGray))
 }
