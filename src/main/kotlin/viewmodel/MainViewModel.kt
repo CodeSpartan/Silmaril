d1c1e8e8daa340c3bc2d8057bb937e2543c8f9bc
CodeSpartan <codespartan.supp@gmail.com>
2025-07-17 16:57:39 +0300

    send multiple commands separated by the ; symbol

--- src/main/kotlin/viewmodel/MainViewModel.kt
+++ src/main/kotlin/viewmodel/MainViewModel.kt
@@ -44,14 +44,29 @@ class MainViewModel(private val client: MudConnection) {
     // Function to send a message via TCP
     fun sendMessage(message: String) {
         // println("Sending: $message")
-        // Adding the sent message to the list locally
-        if (!isEnteringPassword.value)
-            _messages.value += ColorfulTextMessage(arrayOf(TextMessageChunk(AnsiColor.Yellow, AnsiColor.Black, false, ">$message")))
-        else
-            _messages.value += ColorfulTextMessage(arrayOf(TextMessageChunk(AnsiColor.Yellow, AnsiColor.Black, false, ">********")))
+        // @TODO: Integrate this special case into the whole system later. We don't want to break triggers, etc.
+        if (isEnteringPassword.value) {
+            _messages.value += ColorfulTextMessage(arrayOf(TextMessageChunk(AnsiColor.Yellow,AnsiColor.Black,false,"> ********")))
+            client.sendMessage(message)
+            return;
+        }
 
-        // Send the message over TCP asynchronously
-        client.sendMessage(message)
+        // @TODO: Add some class that treats input messages here. If it's a system message, there's a lot to do.
+        // @TODO: Current message.split(;) won't work, it's a temporary solution. We need to separate by ; only if it's not inside {}.
+        // @TODO: Besides, this needs to work: "look;#test" the same as "#test;look", which it currently doesn't.
+        // @TODO: And possibly each separate command needs to go through sendMessage recursively to be displayed correctly.
+        // @TODO: Whether or not to display commands separated by ; on one line or multiple needs to be a configurable option.
+        if (message.startsWith("#")) {
+            // system message, not to be sent
+            _messages.value += ColorfulTextMessage(arrayOf(TextMessageChunk(AnsiColor.White, AnsiColor.Black, true, "> $message")))
+        }
+        else {
+            _messages.value += ColorfulTextMessage(arrayOf(TextMessageChunk(AnsiColor.Yellow, AnsiColor.Black, false, "> $message")))
+            val substrings = message.split(";")
+            for (substring in substrings) {
+                client.sendMessage(substring)
+            }
+        }
     }
 
     fun displaySystemMessage(message: String) {
