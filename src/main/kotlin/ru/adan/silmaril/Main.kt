9fee5ff914192ceefe268e06faab0c5e82794b45
CodeSpartan <codespartan.supp@gmail.com>
2025-08-06 23:50:00 +0300

    moved menubar to a separate file

--- src/main/kotlin/ru/adan/silmaril/Main.kt
+++ src/main/kotlin/ru/adan/silmaril/Main.kt
@@ -1,6 +1,5 @@
 package ru.adan.silmaril
 
-import androidx.compose.material.Icon
 import org.jetbrains.compose.resources.painterResource
 import androidx.compose.ui.window.*
 import androidx.compose.runtime.*
@@ -11,21 +10,12 @@ import java.awt.Dimension
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
 import org.koin.compose.KoinContext
 import org.koin.compose.koinInject
-import org.koin.core.component.get
-import ru.adan.silmaril.model.FileLogger
 import ru.adan.silmaril.model.MapModel
-import ru.adan.silmaril.model.Profile
 import ru.adan.silmaril.generated.resources.Res
 import ru.adan.silmaril.generated.resources.icon
-import ru.adan.silmaril.misc.FontManager
 import ru.adan.silmaril.view.AdditionalOutputWindow
 import ru.adan.silmaril.view.FloatingWindow
 import ru.adan.silmaril.view.HoverManagerProvider
@@ -34,12 +24,9 @@ import ru.adan.silmaril.view.MapWindow
 import ru.adan.silmaril.view.Tab
 import ru.adan.silmaril.view.TabbedView
 import ru.adan.silmaril.view.small_dialogs.ProfileDialog
-import ru.adan.silmaril.misc.capitalized
-import ru.adan.silmaril.visual_styles.StyleManager
 import org.koin.core.context.startKoin
-import org.koin.core.parameter.parametersOf
 import ru.adan.silmaril.model.ProfileManager
-import org.koin.core.component.get
+import ru.adan.silmaril.view.AppMenuBar
 
 fun main() {
     startKoin {
@@ -67,82 +54,27 @@ fun main() {
             var selectedTabIndex by remember { mutableStateOf(0) }
             val showProfileDialog = remember { mutableStateOf(false) }
 
-            fun exitApp() {
-                mapModel.cleanup()
-                profileManager.cleanup()
-                settingsManager.cleanup()
-                exitApplication()
-            }
-
             // Main Window
             Window(
                 onCloseRequest = {
-                    exitApp()
+                    cleanupOnExit(mapModel, profileManager, settingsManager)
+                    exitApplication()
                 },
                 state = mainWindowState,
                 title = "Silmaril",
                 icon = painterResource(Res.drawable.icon),
             ) {
-                MenuBar {
-                    Menu("Файл", mnemonic = 'Ф') {
-                        Item("Выход", mnemonic = 'В') {
-                            exitApp()
-                        }
+                AppMenuBar(
+                    settingsManager = settingsManager,
+                    profileManager = profileManager,
+                    showMapWindow = showMapWindow,
+                    showAdditionalOutputWindow = showAdditionalOutputWindow,
+                    showProfileDialog = showProfileDialog,
+                    onExit = {
+                        cleanupOnExit(mapModel, profileManager, settingsManager)
+                        exitApplication()
                     }
-                    Menu("Вид", mnemonic = 'В') {
-                        Menu("Шрифт", mnemonic = 'Ш') {
-                            FontManager.fontFamilies.keys.filter { it != "RobotoClassic" && it != "SourceCodePro" }
-                                .forEach { key ->
-                                    CheckboxItem(
-                                        text = key,
-                                        checked = settings.font == key,
-                                        onCheckedChange = {
-                                            settingsManager.updateFont(key)
-                                        }
-                                    )
-                                }
-                        }
-                        Menu("Цветовая тема", mnemonic = 'Ц') {
-                            StyleManager.styles.keys.forEach { key ->
-                                CheckboxItem(
-                                    text = key,
-                                    checked = settings.colorStyle == key,
-                                    onCheckedChange = {
-                                        settingsManager.updateColorStyle(key)
-                                    }
-                                )
-                            }
-                        }
-                        CheckboxItem(
-                            text = "Карта",
-                            mnemonic = 'К',
-                            checked = showMapWindow.value,
-                            onCheckedChange = {
-                                showMapWindow.value = it
-                                settingsManager.updateFloatingWindowState("MapWindow", it)
-                            }
-                        )
-                        CheckboxItem(
-                            text = "Окно вывода",
-                            mnemonic = 'О',
-                            checked = showAdditionalOutputWindow.value,
-                            onCheckedChange = {
-                                showAdditionalOutputWindow.value = it
-                                settingsManager.updateFloatingWindowState("AdditionalOutput", it)
-                            }
-                        )
-                        Item("Добавить окно", mnemonic = 'Д', onClick = { showProfileDialog.value = true })
-                    }
-                    Menu(profileManager.currentProfileName.value, mnemonic = profileManager.currentProfileName.value.first()) {
-                        CheckboxItem(
-                            text = "Авто-переподкл.",
-                            mnemonic = 'А',
-                            checked = settingsManager.settings.value.autoReconnect,
-                            onCheckedChange = { settingsManager.toggleAutoReconnect(it) }
-                        )
-                        Item("Группы", mnemonic = 'Г', onClick = { /*showGroupsDialog.value = true*/ })
-                    }
-                }
+                )
                 window.minimumSize = Dimension(800, 600)
 
                 // watch for resize, move, fullscreen toggle and save into settings
@@ -164,31 +96,10 @@ fun main() {
                     tabs = tabs,
                     selectedTabIndex = selectedTabIndex,
                     onTabSelected = { newIndex, tabName ->
-                        profileManager.currentClient.value = profileManager.gameWindows.value[tabName]!!.client
-                        profileManager.currentMainViewModel.value = profileManager.gameWindows.value[tabName]!!.mainViewModel
-                        selectedTabIndex = newIndex
-                        profileManager.currentProfileName.value = tabName.capitalized()
+                        selectedTabIndex = profileManager.tabSwitched(newIndex, tabName)
                     },
                     onTabClose = { index, tabName ->
-                        profileManager.gameWindows.value[tabName]?.onCloseWindow()
-                        profileManager.assignNewWindowsTemp(profileManager.gameWindows.value.filterKeys { it != tabName }.toMap())
-                        // if we're closing the currently opened tab, switch to the first available one
-                        if (index == selectedTabIndex) {
-                            val firstValidProfile = profileManager.gameWindows.value.values.first()
-                            profileManager.currentClient.value = firstValidProfile.client
-                            profileManager.currentMainViewModel.value = firstValidProfile.mainViewModel
-
-                            val firstAvailableTabIndex = tabs.indexOfFirst { it.title == firstValidProfile.profileName }
-                            selectedTabIndex =
-                                if (firstAvailableTabIndex > index) firstAvailableTabIndex - 1 else firstAvailableTabIndex
-                            profileManager.currentProfileName.value = firstValidProfile.profileName.capitalized()
-                        }
-                        // if we're closing a tab to the left of current, the current id will need to be adjusted to the left
-                        else {
-                            if (selectedTabIndex > index) {
-                                selectedTabIndex--
-                            }
-                        }
+                        selectedTabIndex = profileManager.tabClosed(tabs, index, tabName, selectedTabIndex)
                     }
                 )
 
@@ -218,6 +129,12 @@ fun main() {
             }
         }
     }
+}
+
+fun cleanupOnExit(mapModel: MapModel, profileManager: ProfileManager, settingsManager: SettingsManager) {
+    mapModel.cleanup()
+    profileManager.cleanup()
+    settingsManager.cleanup()
 }
 
 @Composable
---
+++ src/main/kotlin/ru/adan/silmaril/view/AppMenuBar.kt
@@ -0,0 +1,86 @@
+package ru.adan.silmaril.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.window.FrameWindowScope
+import androidx.compose.ui.window.MenuBar
+import ru.adan.silmaril.misc.FontManager
+import ru.adan.silmaril.model.ProfileManager
+import ru.adan.silmaril.model.SettingsManager
+import ru.adan.silmaril.visual_styles.StyleManager
+
+@Composable
+fun FrameWindowScope.AppMenuBar(
+    settingsManager: SettingsManager,
+    profileManager: ProfileManager,
+    showMapWindow: MutableState<Boolean>,
+    showAdditionalOutputWindow: MutableState<Boolean>,
+    showProfileDialog: MutableState<Boolean>,
+    onExit: () -> Unit
+) {
+    val settings by settingsManager.settings.collectAsState()
+
+    MenuBar {
+        Menu("Файл", mnemonic = 'Ф') {
+            Item("Выход", mnemonic = 'В') {
+                onExit()
+            }
+        }
+        Menu("Вид", mnemonic = 'В') {
+            Item("Добавить игровое окно", mnemonic = 'Д', onClick = { showProfileDialog.value = true })
+            Separator()
+            Menu("Шрифт", mnemonic = 'Ш') {
+                FontManager.fontFamilies.keys.filter { it != "RobotoClassic" && it != "SourceCodePro" }
+                    .forEach { key ->
+                        CheckboxItem(
+                            text = key,
+                            checked = settings.font == key,
+                            onCheckedChange = {
+                                settingsManager.updateFont(key)
+                            }
+                        )
+                    }
+            }
+            Menu("Цветовая тема", mnemonic = 'Ц') {
+                StyleManager.styles.keys.forEach { key ->
+                    CheckboxItem(
+                        text = key,
+                        checked = settings.colorStyle == key,
+                        onCheckedChange = {
+                            settingsManager.updateColorStyle(key)
+                        }
+                    )
+                }
+            }
+            CheckboxItem(
+                text = "Карта",
+                mnemonic = 'К',
+                checked = showMapWindow.value,
+                onCheckedChange = {
+                    showMapWindow.value = it
+                    settingsManager.updateFloatingWindowState("MapWindow", it)
+                }
+            )
+            CheckboxItem(
+                text = "Окно вывода",
+                mnemonic = 'О',
+                checked = showAdditionalOutputWindow.value,
+                onCheckedChange = {
+                    showAdditionalOutputWindow.value = it
+                    settingsManager.updateFloatingWindowState("AdditionalOutput", it)
+                }
+            )
+        }
+        Menu(profileManager.currentProfileName.value, mnemonic = profileManager.currentProfileName.value.first()) {
+            CheckboxItem(
+                text = "Авто-переподкл.",
+                mnemonic = 'А',
+                checked = settingsManager.settings.value.autoReconnect,
+                onCheckedChange = { settingsManager.toggleAutoReconnect(it) }
+            )
+            Item("Группы", mnemonic = 'Г', onClick = { /*showGroupsDialog.value = true*/ })
+        }
+    }
+}
